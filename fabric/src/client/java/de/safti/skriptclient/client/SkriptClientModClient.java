14181ef2d0765cacd030a412f75ee2dea0071402
chaossafti <felix.marczinzik@gmail.com>
2025-07-16 21:20:56 +0200

    added fabric module

---
+++ fabric/src/client/java/de/safti/skriptclient/client/SkriptClientModClient.java
@@ -0,0 +1,10 @@
+package de.safti.skriptclient.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class SkriptClientModClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
---
+++ fabric/src/main/java/de/safti/skriptclient/SkriptClientMod.java
@@ -0,0 +1,10 @@
+package de.safti.skriptclient;
+
+import net.fabricmc.api.ModInitializer;
+
+public class SkriptClientMod implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
